7e34c2efa3afcff83dd72b1ed8382562959a410b
Coko <91132775+Coko7@users.noreply.github.com>
2025-12-01 07:39:17 +0100

    refactor: add anyhow dep

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,23 +1,27 @@
+use anyhow::{Context, Result, bail};
 use core::panic;
 
 advent_of_code::solution!(1);
 
-fn parse_rotation(value: &str) -> i32 {
-    let dir = value.chars().next().unwrap();
+fn parse_rotation(value: &str) -> Result<i32> {
+    let dir = value.chars().next().context("expects a char")?;
     let mul = match dir {
         'L' => -1,
         'R' => 1,
-        _ => panic!("should not happen"),
+        _ => bail!("invalid rotation char"),
     };
-    let amount = value.strip_prefix(dir).unwrap().parse::<i32>().unwrap();
-    mul * amount
+    let amount = value
+        .strip_prefix(dir)
+        .context("should have prefix")?
+        .parse::<i32>()?;
+    Ok(mul * amount)
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
     let mut zeroed = 0;
     let mut pos = 50;
     for instr in input.lines() {
-        let rot = parse_rotation(instr);
+        let rot = parse_rotation(instr).unwrap();
         pos = (pos + rot).rem_euclid(100);
         if pos == 0 {
             zeroed += 1;
@@ -30,10 +34,9 @@ pub fn part_two(input: &str) -> Option<u64> {
     let mut zeroed = 0;
     let mut pos = 50;
     for instr in input.lines() {
-        let rot = parse_rotation(instr);
+        let rot = parse_rotation(instr).unwrap();
         for _ in 0..rot.abs() {
-            pos += rot.signum();
-            pos = pos.rem_euclid(100);
+            pos = (pos + rot.signum()).rem_euclid(100);
             if pos == 0 {
                 zeroed += 1;
             }
